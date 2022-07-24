0de28ff8871a69458326052d8a7ac0ca5cb2b57e
Edson Alves <edsonapf@gmail.com>
2022-07-24 16:54:53 -0300

    feat: remove ThemeProvider and related files

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
-import { useMemo } from "react";
-import { ThemeProvider } from "styled-components";
 import PokedexProvider from "./context/PokedexContext";
 import CustomRoutes from "./CustomRoutes";
-import useLocalStorage from "./hooks/useLocalStorage";
 import { GlobalStyle } from "./styles/global";
-import themes from "./styles/themes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-// TODO: remove theme
-type Theme = "dark" | "light";
-
-function AppWithTheme() {
-  const [theme, setTheme] = useLocalStorage<Theme>("theme", "dark");
-
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.light;
-  }, [theme]);
-
-  const handleChangeTheme = () => {
-    setTheme((prev: Theme): Theme => (prev === "dark" ? "light" : "dark"));
-  };
-
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <ToastContainer />
-      <CustomRoutes />
-    </ThemeProvider>
-  );
-}
 
 function App() {
   return (
     <PokedexProvider>
-      <AppWithTheme />
+      <GlobalStyle />
+      <ToastContainer />
+      <CustomRoutes />
     </PokedexProvider>
   );
 }
--- src/styles/themes/index.ts
+++
@@ -1,6 +0,0 @@
-import dark from './dark';
-import light from './light';
-
-const themes = { dark, light };
-
-export default themes;
