19f79cbc9c24539e4332fa7e0b09e16f67815e83
Edson Alves <edsonapf@gmail.com>
2022-07-24 16:08:50 -0300

    feat: add Modal on Pokedex page

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -6,14 +6,35 @@ import {
 } from "./styles";
 import PokemonListContainer from "../../components/PokemonListContainer";
 import Card from "../../components/Card";
-import { useContext } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { PokedexContext } from "../../context/PokedexContext";
+import { PokemonDetails } from "../../types/Pokemon";
+import Modal from "../../components/Modal";
+import InfoCard from "../../components/InfoCard";
 
 function Pokedex() {
   const { pokedex } = useContext(PokedexContext);
+  const [selectedPokemon, setSelectedPokemon] = useState<PokemonDetails | null>(
+    null
+  );
+
+  const isModalOpen = useMemo(() => !!selectedPokemon, [selectedPokemon]);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isModalOpen]);
 
   return (
-    <Container>
+    <Container className={isModalOpen ? "modal-open" : ""}>
+      {isModalOpen && (
+        <Modal onCloseModal={() => setSelectedPokemon(null)}>
+          <InfoCard content={selectedPokemon!} />
+        </Modal>
+      )}
       {pokedex.length > 0 ? (
         <PokemonListContainer>
           {pokedex.map((pokemon) => {
@@ -22,9 +43,7 @@ function Pokedex() {
                 content={pokemon}
                 key={pokemon.name}
                 showRemoveFromPokedexButton
-                onMoreInfoButtonPress={() =>
-                  console.log("onMoreInfoButtonPress")
-                }
+                onMoreInfoButtonPress={() => setSelectedPokemon(pokemon)}
               />
             );
           })}
